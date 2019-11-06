db92c6f91a9b4cac40773c11ffea524f285b78d8
Yubraj Pokhrel <yubraj.pokhrel@curantissolutions.com>
2019-11-06 17:07:55 -0600

    refoactor and create page for sqs

--- src/main/java/com/tools/localstackui/LocalStackUiApplication.java
+++ src/main/java/com/tools/localstackui/LocalStackUiApplication.java
@@ -2,6 +2,7 @@ package com.tools.localstackui;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
 public class LocalStackUiApplication {
--- src/main/java/com/tools/localstackui/controller/HomeController.java
+++ src/main/java/com/tools/localstackui/controller/HomeController.java
@@ -1,35 +1,17 @@
 package com.tools.localstackui.controller;
 
-import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
-import com.amazonaws.services.sns.AmazonSNS;
-import com.tools.localstackui.services.SNSService;
-import java.util.List;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
-@Controller
+@Controller()
 public class HomeController {
 
-  @Autowired
-  SNSService snsService;
-
-@RequestMapping(value = "/home", method = RequestMethod.GET)
-public String home(Model model){
+  @RequestMapping(value = "/", method = GET)
+  public String home(Model model) {
     return "index";
-  }
-
-  @GetMapping(value = "/sns-topics/{topicArn}", produces = APPLICATION_JSON_VALUE)
-  public String getIndividualTopic(@PathVariable("topicArn") String topicArn, Model model) {
-    List<String> subscriptions =  snsService.getSubscriptionByTopicName(topicArn);
-    model.addAttribute("snsTopic", topicArn);
-    model.addAttribute("subscriptions", subscriptions);
-    return "indTopicPage";
   }
 
 }
