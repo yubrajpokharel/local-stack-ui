922a8c04df399e7ea508da525855b02a7976ceb8
yubrajpokharel <yubrajpokharel555@gmail.com>
2019-11-05 21:27:17 -0600

    added screenshots and remove environment paramenters

--- src/main/java/com/tools/localstackui/config/AWSConfiguration.java
+++ src/main/java/com/tools/localstackui/config/AWSConfiguration.java
@@ -5,43 +5,41 @@ import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.client.builder.AwsClientBuilder.EndpointConfiguration;
 import com.amazonaws.regions.Regions;
-import com.amazonaws.services.chime.model.Credential;
 import com.amazonaws.services.sns.AmazonSNS;
 import com.amazonaws.services.sns.AmazonSNSClientBuilder;
 import com.amazonaws.services.sqs.AmazonSQS;
 import com.amazonaws.services.sqs.AmazonSQSClientBuilder;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AWSConfiguration {
 
-  @Value("${aws.access.key}")
-  String accessKey;
+  String accessKey = "SOMEACCESSKEY";
 
-  @Value("${aws.secret.key}")
-  String secretKey;
+  String secretKey = "SOMESECRETKEY";
 
   @Bean
-  public AWSCredentials awsCredentials(){
+  public AWSCredentials awsCredentials() {
     return new BasicAWSCredentials(accessKey, secretKey);
   }
 
   @Bean
-  public AmazonSQS amazonSQS(){
+  public AmazonSQS amazonSQS() {
     return AmazonSQSClientBuilder.standard()
         .withCredentials(new AWSStaticCredentialsProvider(awsCredentials()))
-        .withEndpointConfiguration(new EndpointConfiguration("http://localhost:4576", Regions.US_WEST_2.getName()))
+        .withEndpointConfiguration(
+            new EndpointConfiguration("http://localhost:4576", Regions.US_WEST_2.getName()))
         .build();
   }
 
   @Bean
-  public AmazonSNS amazonSNS(){
+  public AmazonSNS amazonSNS() {
 
     return AmazonSNSClientBuilder.standard()
         .withCredentials(new AWSStaticCredentialsProvider(awsCredentials()))
-        .withEndpointConfiguration(new EndpointConfiguration("http://localhost:4575", Regions.US_WEST_2.getName()))
+        .withEndpointConfiguration(
+            new EndpointConfiguration("http://localhost:4575", Regions.US_WEST_2.getName()))
         .build();
   }
 }
